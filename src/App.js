b61eed105034c9d267c1478e99f012ae4e843187
Masoud Karimi <masoudkarimi345@gmail.com>
2022-10-27 18:55:58 -0400

    more react components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,42 @@
 //import logo from './logo.svg';
 import './App.css';
 import { TypeAnimation } from 'react-type-animation'
+import React from 'react';
+import './icons.js'
+
+class MovingText extends React.Component {
+  render () {
+    return (
+      
+
+    <section className ="BioBackground">
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div className ='Bio'>
+        <h1>👋Hi, my name is Masoud!</h1>
+        <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa. </p>
+
+
+
+      </div>
+    </section> )
+  };
+};
+
+class SkillIcons extends React.Component {
+  render () {
+      return (
+          <div>
+      <img className='icon' src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
+      <img className='icon' src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
+      <img className='icon' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
+      <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
+          </div>
+      )
+  }
+}
 
 
 function App() {
@@ -9,36 +45,21 @@ function App() {
       <header className="App-header">
       
         <div className="Intro-Text">
+
         <h1 className='Name'>
-          Masoud Karimi 
+        Masoud Karimi 
         </h1>
+        
         <TypeAnimation className ='Intro-Text' 
-        sequence={['Computer Science Student at the University of Ottawa']}
-        />
-        <img className='icon' src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
-        <img className='icon' src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
-        <img className='icon' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
-        <img className ='icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
+        sequence={['Computer Science Student at the University of Ottawa']}/>
+        
+        <SkillIcons />
+        
+
+        <MovingText />
         </div>
       </header>
-    <section className ="BioBackground">
-      <div className ='Bio'>
-        <h1>👋Hi, my name is Masoud!</h1>
-        <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa. </p>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
 
-
-      </div>
-    </section>
     </div>
   )};
 

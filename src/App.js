acfd5b26b9271a073317788e328241952ff33cfa
Masoud Karimi <masoudkarimi345@gmail.com>
2022-11-01 21:47:14 -0400

    small changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,9 @@ class MovingText extends React.Component {
       <div className ='Bio'>
         <h1 className='bioText' >👋Hi, my name is Masoud!</h1>
         <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa. </p>
+        <hr id ="line"></hr>
       </div>
+      <hr></hr>
     </section> )
   };
 };
@@ -21,10 +23,10 @@ class SkillIcons extends React.Component {
   render () {
       return (
           <div>
-      <img className='icon' src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
-      <img className='icon' src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
-      <img className='icon' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
-      <img className='icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
+      <img className='icon' alt="icon" src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
+      <img className='icon'alt="icon" src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
+      <img className='icon'alt="icon" src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
+      <img className='icon'alt="icon" src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
           </div>
       )
   }
@@ -33,24 +35,25 @@ class SkillIcons extends React.Component {
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
-      
         <div className="Intro-Text">
 
-        <h1 className='Name'>
-        Masoud Karimi 
-        </h1>
+        <h1 className='Name'> Masoud Karimi </h1>
         
+        
+
         <TypeAnimation className ='Intro-Text' 
         sequence={['Computer Science Student at the University of Ottawa']}/>
         
         <SkillIcons />
         
-
-        <MovingText />
+        
+       
         </div>
+      
       </header>
-
+      <MovingText className="secondText"/>
     </div>
   )};
 

ecd5ef1b19b9a6f1314e22e468368fdffdb8d54b
Masoud Karimi <masoudkarimi345@gmail.com>
2022-11-30 18:07:41 -0500

    test

--- Desktop/Web Dev/my-site/src/App.js
+++ Desktop/Web Dev/my-site/src/App.js
@@ -1,44 +1,71 @@
 //import logo from './logo.svg';
+import {NavBar} from './navbar.js'
+import {Projects} from './projects.js'
 import './App.css';
+import './navstyle.css'
 import { TypeAnimation } from 'react-type-animation'
+import React from 'react';
+import './icons.js'
 
+/*class Navigator extends React.Component {
+  render () {
+    return (
+      <ul>
+   
+    )
+  }
+} */
+
+class MovingText extends React.Component {
+  render () {
+    return (
+
+    <section className ="BioBackground">
+      <div className ='Bio'>
+        <h1 className='bioText' >👋Hi, my name is Masoud!</h1>
+        <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa.
+        <br></br><br></br>I have experience working with Python, HTML, CSS and JavaScript </p>
+        
+      </div> 
+      <hr></hr>
+    </section> )
+  };
+};
+// Skills
+class SkillIcons extends React.Component {
+  render () {
+      return (
+          <div>
+      <img className='icon' alt="icon" src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
+      <img className='icon'alt="icon" src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
+      <img className='icon'alt="icon" src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
+      <img className='icon'alt="icon" src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
+          </div>
+      )
+  }
+}
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" href="about">
+
+
+      < NavBar /> 
       <header className="App-header">
-      
         <div className="Intro-Text">
-        <h1 className='Name'>
-          Masoud Karimi 
-        </h1>
+
+        <h1 className='Name'> Masoud Karimi </h1>
+        
         <TypeAnimation className ='Intro-Text' 
-        sequence={['Computer Science Student at the University of Ottawa']}
-        />
-        <img className='icon' src='https://cdn3.iconfinder.com/data/icons/logos-and-brands-adobe/512/267_Python-512.png'/>
-        <img className='icon' src='https://cdn-icons-png.flaticon.com/512/919/919828.png'/>
-        <img className='icon' src='https://cdn.pixabay.com/photo/2017/08/05/11/16/logo-2582748_1280.png' />
-        <img className ='icon' src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png' />
+        sequence={['Computer Science Student at the University of Ottawa']}/>
+        <SkillIcons />
+        
         </div>
+      
       </header>
-    <section className ="BioBackground">
-      <div className ='Bio'>
-        <h1>👋Hi, my name is Masoud!</h1>
-        <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa. </p>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
+      <MovingText className="secondText"/>
 
-
-      </div>
-    </section>
+      <Projects />
     </div>
   )};
 
---
+++ Desktop/Web Dev/my-site/src/projects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+class Projects extends React.Component {
+    render () {
+        return (
+            <div>
+            <h1>Projects</h1>
+
+            <div class ='row-padding'>
+                <div class='w3-container'>
+                </div>
+            </div>
+            </div>
+        )
+    }
+}
+export {Projects};  

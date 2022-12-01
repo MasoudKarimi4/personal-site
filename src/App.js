32b68c443308a74da7191195f380e15bd478d46d
Masoud Karimi <masoudkarimi345@gmail.com>
2022-11-30 19:47:38 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -13,8 +13,8 @@ class MovingText extends React.Component {
   render () {
     return (
     <section className ="BioBackground" href='#about'>
-      <div className ='Bio'>
-      <img className="selfpicture" src="https://i.imgur.com/yjTmZ50.png"/>
+      <a href="#ads"></a>
+      <div className ='Bio'> 
         <h1 className='bioText' >👋Hi, my name is Masoud!</h1>
         <p className="bioText">I am in my first year of the Honours BSc of Computer Science at the University of Ottawa. Currently enrolled in Intro to Computing, Calculus I and Intro to Linear Algebra 
         <br></br><br></br>I have experience working with Python, HTML, CSS and JavaScript </p>

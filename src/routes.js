3a7f146184211ed592c6e163e760e2af97f911ff
Masoud Karimi <masoudkarimi345@gmail.com>
2023-01-11 12:09:26 -0500

    site

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Route, IndexRoute } from 'react-router-dom';
+
+import App from './app.js';
+
+export default (
+    <Route path ='/' component={App}></Route>
+)
